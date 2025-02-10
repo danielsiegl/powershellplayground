ded47e605c8e87fd1d249f147c722375635dfb83
Daniel Siegl <daniel.siegl@lieberlieber.com>
2025-02-10 21:30:24 +0100

    Added Markdown Output to Job summary

--- pslint.ps1
+++ pslint.ps1
@@ -1,6 +1,15 @@
-Set-PSRepository PSGallery -InstallationPolicy Trusted
-Install-Module PSScriptAnalyzer -ErrorAction Stop
+
+# This script is used to lint PowerShell scripts using PSScriptAnalyzer.
+
+# Check if PSScriptAnalyzer is installed, if not install it.
+if (-not (Get-Module -ListAvailable -Name PSScriptAnalyzer)) {
+    Set-PSRepository PSGallery -InstallationPolicy Trusted
+    Install-Module PSScriptAnalyzer -ErrorAction Stop
+}
+
+# Lint the PowerShell scripts.
 Invoke-ScriptAnalyzer -Path *.ps1 -Recurse -Outvariable issues
+
 $errors   = $issues.Where({$_.Severity -eq 'Error'})
 $warnings = $issues.Where({$_.Severity -eq 'Warning'})
 if ($errors) {
