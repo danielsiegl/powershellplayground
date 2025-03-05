9a2b3768089bb2b2f81f4bd3b3f67763593ecb6e
Daniel Siegl <daniel.siegl@lieberlieber.com>
2025-03-05 21:03:07 +0100

    Add tests for CostWindow class and improve existing test setups #13
    Introduce unit tests for the CostWindow class, ensuring cost
    calculations meet specified criteria. Enhance existing tests for the
    bank holidays and person modules by implementing BeforeAll blocks for
    better setup. Add a .gitignore file to exclude test artifacts.

--- tests/bankholidays.tests.ps1
+++ tests/bankholidays.tests.ps1
@@ -1,29 +1,21 @@
-# Load the function to be tested
-using module "./../Modules/bankholidays.psm1"
+# Description: Pester tests for the bankholidays module
 
 Describe "Get-AustrianBankHolidays" {
+    BeforeAll { 
+        . "$PSScriptRoot\..\Modules\bankholidays.ps1"   
+    }
     Context "When called with valid dates" {
         It "Should return an array of holiday objects" {
-            # Mock the Invoke-RestMethod to return a predefined response
-            # Mock -CommandName Invoke-RestMethod -MockWith {
-            #     return @(
-            #         @{ startDate = "2023-01-01"; Name = @{ text = "Neujahr" } },
-            #         @{ startDate = "2023-05-01"; Name = @{ text = "Staatsfeiertag" } },
-            #         @{ startDate = "2023-12-25"; Name = @{ text = "Weihnachten" } }
-            #     )
-            # }
-
+           
             $result = Get-AustrianBankHolidays -StartDate "2023-01-01" -EndDate "2023-12-31"
-            $result.Count | Should -Be 14
-            # $result[0].Date | Should -Be "2023-01-01"
+            $result.Count | Should -Be 15
+            $result[0].Date | Should -Be "2023-01-01"
         }
     }
 
     Context "When API call fails" {
         It "Should write an error message and return nothing" {
-            # Mock the Invoke-RestMethod to throw an exception
-            # Mock -CommandName Invoke-RestMethod -MockWith { throw "API call failed" }
-
+           
             { Get-AustrianBankHolidays -StartDate "XXX" -EndDate "2023-12-31" } | Should -Throw
         }
     }
